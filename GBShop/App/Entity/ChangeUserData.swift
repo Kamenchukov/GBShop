f9a3dedca78460c5ffcae346fd074e3d88ec685d
Constantin <k.kamenchukov@gmail.com>
2022-03-09 12:19:18 +0300

    Update_2_solved_problems

---
+++ GBShop/App/Entity/ChangeUserData.swift
@@ -0,0 +1,8 @@
+//
+//  changeUserData.swift
+//  GBShop
+//
+//  Created by Константин Каменчуков on 09.03.2022.
+//
+
+import Foundation
---
+++ GBShop/App/Entity/RegisterUser.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterUser.swift
+//  GBShop
+//
+//  Created by Константин Каменчуков on 07.03.2022.
+//
+
+import Foundation
