5d561b5f1bf52ad89ddac9950ac97953027b52be
Constantin <k.kamenchukov@gmail.com>
2022-03-16 17:16:20 +0300

    HW_3

---
+++ GBShop/App/Entity/CatalogResult.swift
@@ -0,0 +1,8 @@
+//
+//  CatalogResult.swift
+//  GBShop
+//
+//  Created by Константин Каменчуков on 16.03.2022.
+//
+
+import Foundation
---
+++ GBShop/App/Entity/GetGoodByIdResult.swift
@@ -0,0 +1,22 @@
+//
+//  GetGoodById.swift
+//  GBShop
+//
+//  Created by Константин Каменчуков on 16.03.2022.
+//
+
+import Foundation
+
+struct GetGoodById: Codable {
+    let result: Int
+    let productName: String
+    let productPrice: Int
+    let productDescription: String
+    
+    enum CodingKeys: String, CodingKey {
+        case result = "result"
+        case productName = "product_name"
+        case productPrice = "product_price"
+        case productDescription = "product_description"
+    }
+}
